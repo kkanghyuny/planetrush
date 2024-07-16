c78e2bb3e595ffc367686b23697801d903ae7327
심종한 <simhani1@gmail.com>
2024-07-16 13:39:55 +0900

    feat: 프로젝트 생성

---
+++ Backend/planetrush/src/main/java/com/planetrush/planetrush/PlanetrushApplication.java
@@ -0,0 +1,13 @@
+package com.planetrush.planetrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetrushApplication.class, args);
+	}
+
+}
