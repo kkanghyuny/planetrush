566a88e232645bb2df6649ef9649efb0807fe2a2
김경민 <yogurt2819@gmail.com>
2024-07-19 15:15:59 +0900

    hotfix: repository 어노테이션 제거

--- Backend/planetrush/src/main/java/com/planetrush/planetrush/planet/repository/PlanetRepository.java
+++ Backend/planetrush/src/main/java/com/planetrush/planetrush/planet/repository/PlanetRepository.java
@@ -1,10 +1,8 @@
 package com.planetrush.planetrush.planet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.planetrush.planetrush.planet.domain.Planet;
 
-@Repository
 public interface PlanetRepository extends JpaRepository<Planet, Long> {
 }
