5d4c82ab2c5d4c0fa16426c89f831ff2a2b35ff1
김도은 <ide05484@naver.com>
2024-08-08 13:04:30 +0900

    fix: server_url 변경

--- Frontend/src/store/urlStore.jsx
+++ Frontend/src/store/urlStore.jsx
@@ -5,7 +5,7 @@ const SERVER_URL = "http://i11a509.p.ssafy.io:8002/api/v1";
 const LOCAL_URL = "http://70.12.247.69:8080/api/v1";
 
 const useURLStore = create((set) => ({
-  SERVER_URL: DEV_URL,
+  SERVER_URL: SERVER_URL,
   setSERVERURL: (newURL) => set({ SERVER_URL: newURL }),
 }));
 
