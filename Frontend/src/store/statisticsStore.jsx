0001ae0cf2167e0e9b509214b43e2a58d67ba795
김도은 <ide05484@naver.com>
2024-08-12 13:23:30 +0900

    fix: 랜덤 프로필, 입력창 고정

--- Frontend/src/store/statisticsStore.jsx
+++ Frontend/src/store/statisticsStore.jsx
@@ -3,7 +3,8 @@ import create from "zustand";
 const useStatisticsStore = create((set) => ({
   challengeCnt: 0,
   completionCnt: 0,
-  setStatistics: (challengeCnt, completionCnt) => set({ challengeCnt, completionCnt }),
+  setStatistics: (challengeCnt, completionCnt) =>
+    set({ challengeCnt, completionCnt }),
 }));
 
 export default useStatisticsStore;
