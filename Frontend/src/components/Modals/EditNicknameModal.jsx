f645a52f12fda60c2cbbd192a9ad69aa090acd83
AhnHongchan <hha0408@gmail.com>
2024-08-12 13:29:03 +0900

    fix: 회원탈퇴 시 처음 화면으로 보내기

--- Frontend/src/components/Modals/EditNicknameModal.jsx
+++ Frontend/src/components/Modals/EditNicknameModal.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Cookies from "js-cookie";
 import instance from "../../pages/AuthenticaitionPage/Axiosinstance";
 import useStatisticsStore from "../../store/statisticsStore";
@@ -6,7 +8,7 @@ import useStatisticsStore from "../../store/statisticsStore";
 const NicknameEditModal = ({ nickname, isOpen, closeModal, saveNickname }) => {
   const [newNickname, setNewNickname] = useState("");
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
-
+  const navigate = useNavigate();
   const challengeCnt = useStatisticsStore((state) => state.challengeCnt); // challengeCnt 가져오기
   const completionCnt = useStatisticsStore((state) => state.completionCnt); // completionCnt 가져오기
 
@@ -22,7 +24,7 @@ const NicknameEditModal = ({ nickname, isOpen, closeModal, saveNickname }) => {
       setNewNickname("");
     } else {
       saveNickname(newNickname);
-      Cookies.set('nickname', newNickname);
+      Cookies.set("nickname", newNickname);
       closeModal();
     }
   };
@@ -32,17 +34,18 @@ const NicknameEditModal = ({ nickname, isOpen, closeModal, saveNickname }) => {
       const response = await instance.patch("/members/auth/exit");
 
       if (response.status === 200) {
-        Cookies.remove('access-token');
-        Cookies.remove('refresh-token');
-        Cookies.remove('nickname');
+        Cookies.remove("access-token");
+        Cookies.remove("refresh-token");
+        Cookies.remove("nickname");
 
-        console.log('회원 탈퇴가 성공적으로 처리되었습니다.');
-        closeModal(); 
+        navigate("/");
+        console.log("회원 탈퇴가 성공적으로 처리되었습니다.");
+        closeModal();
       } else {
-        console.log('회원 탈퇴 요청이 성공하지 않았습니다.');
+        console.log("회원 탈퇴 요청이 성공하지 않았습니다.");
       }
     } catch (error) {
-      console.error('회원 탈퇴 중 오류가 발생했습니다:', error);
+      console.error("회원 탈퇴 중 오류가 발생했습니다:", error);
     }
   };
 
@@ -71,7 +74,10 @@ const NicknameEditModal = ({ nickname, isOpen, closeModal, saveNickname }) => {
         <button onClick={closeModal}>취소</button>
 
         <h4>회원 탈퇴</h4>
-        <p>지금까지 총 <strong>{challengeCnt}</strong>개의 챌린지에 도전했고, 그 중 <strong>{completionCnt}</strong>개를 성공하셨습니다.</p>
+        <p>
+          지금까지 총 <strong>{challengeCnt}</strong>개의 챌린지에 도전했고, 그
+          중 <strong>{completionCnt}</strong>개를 성공하셨습니다.
+        </p>
         <button onClick={handleOpenConfirm}>예</button>
         <button onClick={closeModal}>아니오</button>
       </div>
