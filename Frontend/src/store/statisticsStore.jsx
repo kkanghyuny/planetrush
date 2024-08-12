d498af7346fc41969de94b53772a4fec5d1a3b6d
AhnHongchan <hha0408@gmail.com>
2024-08-12 12:44:39 +0900

    fix: 마이 페이지 로직 오류 해결

---
+++ Frontend/src/store/statisticsStore.jsx
@@ -0,0 +1,9 @@
+import create from "zustand";
+
+const useStatisticsStore = create((set) => ({
+  challengeCnt: 0,
+  completionCnt: 0,
+  setStatistics: (challengeCnt, completionCnt) => set({ challengeCnt, completionCnt }),
+}));
+
+export default useStatisticsStore;
