d776ed1a195648573f8d15f4d4230be73aedc9f4
kkanghyuny <lkh000131@naver.com>
2024-08-14 13:19:53 +0900

    fix: 오늘 인증을 완료한 사용자가 재요청시 예외 처리

--- Backend/planetrush/src/main/java/com/planetrush/planetrush/verification/facade/VerificationFacade.java
+++ Backend/planetrush/src/main/java/com/planetrush/planetrush/verification/facade/VerificationFacade.java
@@ -7,7 +7,9 @@ import org.springframework.web.multipart.MultipartFile;
 import com.planetrush.planetrush.infra.flask.util.FlaskUtil;
 import com.planetrush.planetrush.infra.s3.S3ImageService;
 import com.planetrush.planetrush.infra.s3.dto.FileMetaInfo;
+import com.planetrush.planetrush.verification.exception.AlreadyVerifiedException;
 import com.planetrush.planetrush.verification.facade.dto.VerifyChallengeDto;
+import com.planetrush.planetrush.verification.service.GetTodayRecordService;
 import com.planetrush.planetrush.verification.service.VerificationService;
 import com.planetrush.planetrush.verification.service.dto.FlaskResponseDto;
 import com.planetrush.planetrush.verification.service.dto.VerificationResultDto;
@@ -21,6 +23,7 @@ public class VerificationFacade {
 
 	private final S3ImageService s3ImageService;
 	private final VerificationService verificationService;
+	private final GetTodayRecordService getTodayRecordService;
 	private final FlaskUtil flaskUtil;
 
 	/**
@@ -31,6 +34,9 @@ public class VerificationFacade {
 	 * @return 인증 여부, 유사도
 	 */
 	public VerifyChallengeDto saveImgAndVerifyChallenge(MultipartFile verificationImg, Long memberId, Long planetId) {
+		if(getTodayRecordService.getTodayRecord(memberId, planetId)) {
+			throw new AlreadyVerifiedException("Member: " + memberId + ", Planet : " + planetId + " already verified today");
+		}
 		FileMetaInfo fileMetaInfo = s3ImageService.uploadVerificationImg(verificationImg, memberId);
 		String standardImgUrl = verificationService.getStandardImgUrlByPlanetId(planetId);
 		String targetImgUrl = fileMetaInfo.getUrl();
